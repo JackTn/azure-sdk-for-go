47a0ab7ceb419da64cc62d2f36a28239c78797ef
v-tianxi <v-tianxi@microsoft.com>
2025-03-21 18:52:52 +0800

    add log

--- eng/tools/generator/cmd/v2/automation/automationCmd.go
+++ eng/tools/generator/cmd/v2/automation/automationCmd.go
@@ -52,6 +52,7 @@ func Command() *cobra.Command {
 
 func execute(inputPath, outputPath, goVersion string) error {
 	log.Printf("Reading generate input file from '%s'...", inputPath)
+	log.Printf("testllog")
 	fmt.Errorf("test without prefiex [error]")
 	input, err := pipeline.ReadInput(inputPath)
 	if err != nil {
